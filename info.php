061d078dd4c819d596c195efbf0d36642d715449
pedrolaxe <pedrolaxe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-07 21:29:35 +0000

    enviando 1.2
    git-svn-id: https://plugins.svn.wordpress.org/playerzbr/tags/1.2@677692 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ info.php
@@ -0,0 +1,40 @@
+
+<div class="corpo-info" style="margin-left:10px; margin-top:10px;">
+
+<h1 align="center">Plugin Information</h1>
+<p align="center">
+<table width="250" border="0">
+  <tr>
+    <td><strong>Plugin Developer:</strong></td>
+    <td>Pedro Laxe</td>
+  </tr>
+  <tr>
+    <td><strong>Plugin Version:</strong></td>
+    <td>1.2</td>
+  </tr>
+  <tr>
+    <td><strong>Wordpress version:</strong></td>
+    <td><?php global $wp_version; echo $wp_version; ?></td>
+  </tr>
+  <tr>
+    <td><strong>MySQL Version:</strong></td>
+    <td><?php echo mysql_get_server_info(); ?></td>
+  </tr>
+  <tr>
+    <td><strong>PHP Version:</strong></td>
+    <td><?php echo phpversion(); ?></td>
+  </tr>
+   <tr>
+</table>
+<br />
+<table width="800" border="0">
+  <tr>
+  <td><font size="2"><strong>Usage methods:</strong></font></td>
+  <td><font size="2">Text mode: &#91;playerzbr&#93; &nbsp;&nbsp;|&nbsp;&nbsp; PHP mode: &#60;?php echo do_shortcode( '&#91;playerzbr&#93;' ); ?&#62; </font></td>
+ </tr>
+ </table><br />
+<br />
+
+<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=6WQ566DAC4YF8"><img src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" alt="donate"></a>
+</p>
+</div>
