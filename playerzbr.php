7ca66ec0a59b36eccd6e6662909eb340457ad727
pedrolaxe <pedrolaxe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-12 18:25:26 +0000

    1.4 version
    git-svn-id: https://plugins.svn.wordpress.org/playerzbr/trunk@1043625 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- playerzbr.php
+++ playerzbr.php
@@ -2,15 +2,13 @@
 /*
 Plugin Name: PlayerZBR
 Description: PlayerZBR is a player HTML 5 audio streaming.
-Version: 1.3
+Version: 1.4
 Author: Pedro Laxe
 Author URI: http://www.phpsec.com.br/
 License: GPLv2
-
 */
-
 /*      
- *      Copyright 2013 Pedro Laxe <pedrolaxe@gmail.com>
+ *      Copyright 2014 Pedro Laxe <pedrolaxe@gmail.com>
  *      
  *      This program is free software; you can redistribute it and/or modify
  *      it under the terms of the GNU General Public License as published by
@@ -26,15 +24,12 @@ License: GPLv2
  *      along with this program; if not, write to the Free Software
  *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  *      MA 02110-1301, USA.
-
  */
-
+$versionzbr = '1.4';
 add_action( 'init', 'plx_option' );
-
 function plx_option(){
-
 /**
- * Function Activeate PlayerZBR 
+ * Function Activate PlayerZBR 
  *
  * @since 1.0
  *
@@ -49,78 +44,50 @@ register_activation_hook( __FILE__, 'plx_install_hook' );
  *
  */
 function plx_install_hook() {
-
-
   if ( version_compare( PHP_VERSION, '5.2.1', '<' )
-
     or version_compare( get_bloginfo( 'version' ), '3.3', '<' ) ) {
-
       deactivate_plugins( basename( __FILE__ ) );
-
   }
-
   // Vamos criar um opção para ser guardada na base-de-dados
-
   // e incluir um valor por defeito.
-
   add_option( 'plx_option', 'valor_por_defeito' );
-
 }
 
 }
-/**
- * Function Add Styles in Header
- *
- * @since 1.3
- *
- */
-function Addstyles(){
-	$stl = '
-<style type="text/css">
-/* PlayerZBR CSS */
-audio #pl1{
-	width:'.get_option('wid').'px;
-    height:auto;
-		  }
-
-audio{
-	width:'.get_option('wid').'px;
-    height:auto;
-	 }
-
-</style>
-';
-	echo $stl;
-}
-add_action('wp_head', 'Addstyles');
 /**
  * Function to Create PlayerZBR 
  *
  * @since 1.0
  *
+ * Update 1.4 - Create multiples players on shortcode method
  */
-function add_player(){
-
-	$player1 = '
-
+ function ver_autoplay($autoplay){
+	 if($autoplay=="no"){
+		 }elseif($autoplay=="yes"){
+			 return "autoplay";
+			 }
+ }
+function add_player($atts, $content = null) {
+    extract(shortcode_atts(array(
+        'url' => '',
+		'autoplay' => '',
+                    ), $atts));
+    if (empty($url)) {
+        return '<div style="color:red;font-weight:bold;">Playerzbr Error! You must enter the mp3 file URL OR URL of streaming parameter in this shortcode. Please check the documentation and correct the mistake.</div>';
+    }if(empty($autoplay)){
+		$autoplay = 'no';
+	}
+	$player = '
 <div class="playerzbr">
-
-	<audio controls '.get_option('autoplay').'>
-
-  <source src="'.get_option('url').'" type="audio/mpeg">
-
-  <source src="'.get_option('url').'" type="audio/ogg">
-
+	<audio controls '.ver_autoplay($autoplay).'>
+  <source src="'.$url.'" type="audio/mpeg">
+  <source src="'.$url.'" type="audio/ogg">
   Your browser does not support the audio element.
-
 	</audio>
 
 </div>
-
 	';
-
-	return $player1;
-
+	return $player;
 }
 /**
  * Admin Page Functions
@@ -131,31 +98,15 @@ function add_player(){
 if ( is_admin() )
 {
 	add_action('admin_menu', 'plx_pagina_opcoes');
-
 }
 /**
  * Function add Page Options in WP Menu 
  *
  * @since 1.0
  *
- */
+*/
 function plx_pagina_opcoes() {
-
   add_menu_page( 'PlayerZBR', 'PlayerZBR', 'manage_options', 'PlayerZBR-options', 'plx_pagina_opcoes_content' );
-
-  add_submenu_page('PlayerZBR-options', 'Plugin Information', 'Plugin Information', 8, 'PlayerZBR-info' ,'plx_pag_info');  
-
-}
-/**
- * Function include PlayerZBR Info Page
- *
- * @since 1.2
- *
- */
-function plx_pag_info(){
-
-	include "info.php";
-
 }
 
 /**
@@ -164,9 +115,44 @@ function plx_pag_info(){
  * @since 1.0
  *
  */
-
 add_shortcode('playerzbr', 'add_player');
 
+/**
+ * Styles in Header
+ *
+ * @since 1.4
+ *
+ */
+function StylesinHeader(){
+	echo '
+	<style type="text/css">
+audio
+{
+-webkit-transition:all 0.5s linear;
+-moz-transition:all 0.5s linear;
+-o-transition:all 0.5s linear;
+transition:all 0.5s linear;
+-moz-box-shadow: 2px 2px 4px 0px #006773;
+-webkit-box-shadow:  2px 2px 4px 0px #006773;
+box-shadow: 2px 2px 4px 0px #006773;
+-moz-border-radius:7px 7px 7px 7px ;
+-webkit-border-radius:7px 7px 7px 7px ;
+border-radius:7px 7px 7px 7px ;
+}
+audio:hover, audio:focus, audio:active
+{
+-webkit-box-shadow: 15px 15px 20px rgba(0,0, 0, 0.4);
+-moz-box-shadow: 15px 15px 20px rgba(0,0, 0, 0.4);
+box-shadow: 15px 15px 20px rgba(0,0, 0, 0.4);
+-webkit-transform: scale(1.05);
+-moz-transform: scale(1.05);
+transform: scale(1.05);
+}
+	</style>
+	
+	';
+}
+add_action('wp_head','StylesinHeader');
 /**
  * Admin Page Options
  *
@@ -175,54 +161,53 @@ add_shortcode('playerzbr', 'add_player');
  */
 
 function plx_pagina_opcoes_content() {
-
 ?>
-<link rel="stylesheet" href="<?php echo plugins_url( 'js/bootstrap.min.css' , __FILE__ ); ?>" type="text/css">
-<script type="text/javascript" src="<?php echo plugins_url( 'js/bootstrap.min.js' , __FILE__ ); ?>"></script>
+
 <div class="corpo">
-  <?php screen_icon( 'plugins' ); ?>
-  <h2>PlayerZBR Options</h2>
-  <form method="post" action="options.php">
-    <?php wp_nonce_field('update-options') ?>
-    <p>
-    <table width="800" border="0">
-      <tr>
-        <td><font size="2"><strong>URL of Streaming:</strong></font></td>
-        <td><font size="2">
-          <input type="text" name="url" class="input-xxlarge" placeholder="You URL Here" value="<?php echo get_option('url'); ?>" />
-          </font></td>
-      </tr>
-      <tr>
-        <td><font size="2"><strong>Autoplay:</strong></font></td>
-        <td><font size="2">
-          <select name="autoplay" class="input-mini">
-            <option value="autoplay" <?php if(get_option('autoplay')=="autoplay"){echo 'selected="selected"';} ?>>yes</option>
-            <option value="" <?php if(get_option('autoplay')==""){echo 'selected="selected"';} ?>>no</option>
-          </select>
-          &nbsp;Choice for yes or no</font></td>
-      </tr>
-      <tr>
-        <td><font size="2"><strong>Width of Player:</strong></font></td>
-        <td><font size="2">
-          <input type="text" name="wid" class="input-mini" maxlength="4" placeholder="400" value="<?php echo get_option('wid');?>" />
-          PX</font></td>
-      </tr>
-      <br />
-      <tr>
-        <td><font size="2">
-          <input type="submit" name="botenvia" value="Save Modifications" class="btn btn-primary" />
-          </font></td>
-      </tr>
-    </table>
-    <br />
-    <br />
-    </font>
-    </p>
-    <input type="hidden" name="action" value="update" />
-    <input type="hidden" name="page_options" value="url,autoplay,wid" />
-  </form>
+<?php screen_icon( 'plugins' ); ?>
+<h2>PlayerZBR Options</h2>
+<div class="corpo-info" style="margin-left:10px; margin-top:10px;">
+  <p align="center">
+  <table width="250" border="0">
+    <tr>
+      <td><strong>Plugin Developer:</strong></td>
+      <td>Pedro Laxe</td>
+    </tr>
+    <tr>
+      <td><strong>Plugin Version:</strong></td>
+      <td><?php global $versionzbr; echo $versionzbr; ?></td>
+    </tr>
+    <tr>
+      <td><strong>Wordpress version:</strong></td>
+      <td><?php global $wp_version; echo $wp_version; ?></td>
+    </tr>
+    <tr>
+      <td><strong>MySQL Version:</strong></td>
+      <td><?php echo mysql_get_server_info(); ?></td>
+    </tr>
+    <tr>
+      <td><strong>PHP Version:</strong></td>
+      <td><?php echo phpversion(); ?></td>
+    </tr>
+    <tr>
+  </table>
   <br />
-  <p>Developed by Pedro Laxe &copy; <?php echo date('Y'); ?> - <a href="http://twitter.com/pedrolaxe">Twitter</a>&nbsp;- <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=6WQ566DAC4YF8">Donate Here</a></p>
+  <table width="800" border="0">
+    <tr>
+      <td><font size="2"><strong>Usage methods:</strong></font></td>
+      <td><p><font size="2">Text mode: &#91;playerzbr&#93; &nbsp;&nbsp;|&nbsp;&nbsp; PHP mode: &#60;?php echo do_shortcode( '&#91;playerzbr&#93;' ); ?&#62;</font></p></td>
+    </tr>
+    <tr>
+      <td><strong>Options:</strong></td>
+      <td><p>For Autoplay option use: [playerzbr url="YOUR URL" autoplay="yes"]<br>
+		For Music only use: [playerzbr url="Your URL"]
+	 </p></td>
+    </tr>
+  </table>
+  <br />
+  <br />
+  <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=6WQ566DAC4YF8"><img src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" alt="donate"></a>
+  </p>
 </div>
 <?php
 }
--- uninstall.php
+++ uninstall.php
@@ -1,11 +1,8 @@
-<?php
-
-// Vamos garantir que é o WordPress que chama este ficheiro
-// e que realmente está a desistalar o plugin.
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
-  die();
-
-// Vamos remover as opções que criámos na instalação
-delete_option( 'plx_option' );
-
-?>
+<?php
+// Vamos garantir que é o WordPress que chama esse arquivo
+// e que realmente vai desistalar meu plugin.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
+  die();
+// Vamos remover as opções que criamos na instalação
+delete_option( 'plx_option' );
+?>
