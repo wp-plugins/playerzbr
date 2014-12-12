f59f8348bf8bce13f67ad1b9a49e16e40f644929
pedrolaxe <pedrolaxe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-12 18:30:03 +0000

    new version
    git-svn-id: https://plugins.svn.wordpress.org/playerzbr/trunk@1043627 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Vamos garantir que é o WordPress que chama esse arquivo
-// e que realmente vai desistalar meu plugin.
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
-  die();
-// Vamos remover as opções que criamos na instalação
-delete_option( 'plx_option' );
-?>
