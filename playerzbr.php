b7cd4f5d6e3c800dba5c764fe236d2395274bdf5
pedrolaxe <pedrolaxe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-29 16:39:50 +0000

    new version 1.5 released by pedrolaxe
    git-svn-id: https://plugins.svn.wordpress.org/playerzbr/trunk@1149223 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- playerzbr.php
+++ playerzbr.php
@@ -1,166 +1,294 @@
 <?php
+
 /*
+
 Plugin Name: PlayerZBR
-Description: PlayerZBR is a player HTML 5 audio streaming.
-Version: 1.4
+
+Description: PlayerZBR is the best HTML5 responsive player.
+
+Version: 1.5
+
 Author: Pedro Laxe
+
 Author URI: http://www.phpsec.com.br/
+
 License: GPLv2
+
 */
+
 /*      
- *      Copyright 2014 Pedro Laxe <pedrolaxe@gmail.com>
+
+ *      Copyright 2015 Pedro Laxe <pedrolaxe@gmail.com>
+
  *      
+
  *      This program is free software; you can redistribute it and/or modify
+
  *      it under the terms of the GNU General Public License as published by
+
  *      the Free Software Foundation; either version 3 of the License, or
+
  *      (at your option) any later version.
+
  *      
+
  *      This program is distributed in the hope that it will be useful,
+
  *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+
  *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+
  *      GNU General Public License for more details.
+
  *      
+
  *      You should have received a copy of the GNU General Public License
+
  *      along with this program; if not, write to the Free Software
+
  *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+
  *      MA 02110-1301, USA.
+
  */
-$versionzbr = '1.4';
+
+$versionzbr = '1.5';
+
 add_action( 'init', 'plx_option' );
+
 function plx_option(){
+
 /**
+
  * Function Activate PlayerZBR 
+
  *
+
  * @since 1.0
+
  *
+
  */
+
+
 
 register_activation_hook( __FILE__, 'plx_install_hook' );
 
+
+
 /**
+
  * Function Compare Version of WP else Desactive Plugin
+
  *
+
  * @since 1.0
+
  *
+
  */
+
 function plx_install_hook() {
+
   if ( version_compare( PHP_VERSION, '5.2.1', '<' )
+
     or version_compare( get_bloginfo( 'version' ), '3.3', '<' ) ) {
+
       deactivate_plugins( basename( __FILE__ ) );
+
   }
+
   // Vamos criar um opção para ser guardada na base-de-dados
+
   // e incluir um valor por defeito.
+
   add_option( 'plx_option', 'valor_por_defeito' );
-}
 
 }
+
+
+
+}
+
 /**
+
  * Function to Create PlayerZBR 
+
  *
+
  * @since 1.0
+
  *
+
  * Update 1.4 - Create multiples players on shortcode method
+
  */
+
  function ver_autoplay($autoplay){
+
 	 if($autoplay=="no"){
 		 }elseif($autoplay=="yes"){
-			 return "autoplay";
-			 }
+		 	return "autoplay";
+		}
+
  }
+
 function add_player($atts, $content = null) {
+
     extract(shortcode_atts(array(
+
         'url' => '',
+
 		'autoplay' => '',
+
                     ), $atts));
+
     if (empty($url)) {
+
         return '<div style="color:red;font-weight:bold;">Playerzbr Error! You must enter the mp3 file URL OR URL of streaming parameter in this shortcode. Please check the documentation and correct the mistake.</div>';
+
     }if(empty($autoplay)){
+
 		$autoplay = 'no';
+
 	}
+
 	$player = '
+
 <div class="playerzbr">
 	<audio controls '.ver_autoplay($autoplay).'>
   <source src="'.$url.'" type="audio/mpeg">
   <source src="'.$url.'" type="audio/ogg">
   Your browser does not support the audio element.
+
 	</audio>
 
+
+
 </div>
+
 	';
+
 	return $player;
+
 }
+
 /**
+
  * Admin Page Functions
+
  *
+
  * @since 1.0
+
  *
+
  */
+
 if ( is_admin() )
+
 {
+
 	add_action('admin_menu', 'plx_pagina_opcoes');
+
 }
+
 /**
+
  * Function add Page Options in WP Menu 
+
  *
+
  * @since 1.0
+
  *
+
 */
+
 function plx_pagina_opcoes() {
+
   add_menu_page( 'PlayerZBR', 'PlayerZBR', 'manage_options', 'PlayerZBR-options', 'plx_pagina_opcoes_content' );
+
 }
 
 /**
- * Function add Shortcode of PlayerZBR 
- *
- * @since 1.0
- *
- */
-add_shortcode('playerzbr', 'add_player');
-
-/**
- * Styles in Header
- *
- * @since 1.4
- *
- */
-function StylesinHeader(){
-	echo '
-	<style type="text/css">
-audio
+*
+* Scripts on Footer
+*
+* @since 1.5
+*/
+function Activate_player() {
+    echo '
+<script type="text/javascript">
+$( "audio" ).audioPlayer(
 {
--webkit-transition:all 0.5s linear;
--moz-transition:all 0.5s linear;
--o-transition:all 0.5s linear;
-transition:all 0.5s linear;
--moz-box-shadow: 2px 2px 4px 0px #006773;
--webkit-box-shadow:  2px 2px 4px 0px #006773;
-box-shadow: 2px 2px 4px 0px #006773;
--moz-border-radius:7px 7px 7px 7px ;
--webkit-border-radius:7px 7px 7px 7px ;
-border-radius:7px 7px 7px 7px ;
-}
-audio:hover, audio:focus, audio:active
-{
--webkit-box-shadow: 15px 15px 20px rgba(0,0, 0, 0.4);
--moz-box-shadow: 15px 15px 20px rgba(0,0, 0, 0.4);
-box-shadow: 15px 15px 20px rgba(0,0, 0, 0.4);
--webkit-transform: scale(1.05);
--moz-transform: scale(1.05);
-transform: scale(1.05);
-}
-	</style>
+    classPrefix: "audioplayer",
+    strPlay: "Play",
+    strPause: "Pause",
+    strVolume: "Volume"
+});
+</script>
 	
 	';
 }
-add_action('wp_head','StylesinHeader');
+add_action('wp_footer', 'Activate_player');
 /**
- * Admin Page Options
+
+ * Function add Shortcode of PlayerZBR 
+
  *
+
  * @since 1.0
+
  *
+
  */
 
+add_shortcode('playerzbr', 'add_player');
+
+/**
+*
+* Function Add Scripts
+*
+* @since 1.5
+*/
+function Playerzbr_scripts()
+{
+    // Deregister the included library
+    wp_deregister_script( 'jquery' );
+     
+    // Register the library again from Google's CDN
+    wp_register_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js', array(), null, false );
+     
+    // Register the script like this for a plugin:
+    wp_register_script( 'playerzbr-script', plugins_url( '/js/playerzbr.js', __FILE__ ), array( 'jquery' ) );
+    // or
+    // Register the script like this for a theme:
+    wp_register_script( 'playerzbr-scriptt', get_template_directory_uri() . '/js/playerzbr.js', array( 'jquery' ) );
+ 
+	wp_register_style('playerzbr-script', plugins_url('css/playerzbr.css',__FILE__ ));
+	wp_enqueue_style('playerzbr-script');
+    // For either a plugin or a theme, you can then enqueue the script:
+    wp_enqueue_script( 'playerzbr-script' );
+	
+}
+add_action( 'wp_enqueue_scripts', 'Playerzbr_scripts' );
+/**
+
+ * Admin Page Options
+
+ *
+
+ * @since 1.0
+
+ *
+
+ */
+
+
+
 function plx_pagina_opcoes_content() {
+
 ?>
 
 <div class="corpo">
@@ -200,8 +328,7 @@ function plx_pagina_opcoes_content() {
     <tr>
       <td><strong>Options:</strong></td>
       <td><p>For Autoplay option use: [playerzbr url="YOUR URL" autoplay="yes"]<br>
-		For Music only use: [playerzbr url="Your URL"]
-	 </p></td>
+          For Music only use: [playerzbr url="Your URL"] </p></td>
     </tr>
   </table>
   <br />
